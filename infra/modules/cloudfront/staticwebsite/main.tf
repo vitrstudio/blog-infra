de0ad99fd96037c5188bbc1f6325f5678b8a3c4c
Jordi Vilagut Treserra <vilagutjordi@gmail.com>
2025-08-09 14:32:02 +0200

    Fix s3 domain name

--- infra/modules/cloudfront/staticwebsite/main.tf
+++ infra/modules/cloudfront/staticwebsite/main.tf
@@ -4,7 +4,7 @@ resource "aws_cloudfront_distribution" "static_cdn" {
   default_root_object = "index.html"
 
   origin {
-    domain_name              = "blog.${var.s3_domain_name}"
+    domain_name              = var.s3_domain_name
     origin_id                = "s3-origin"
     origin_access_control_id = var.oac_id
   }
